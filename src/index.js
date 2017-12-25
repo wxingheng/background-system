31faee3f3b385a70120c315cce6c40cde2dd3c4a
roger <1664465219@qq.com>
2017-12-25 18:52:09 +0800

    添加bootstrap和font-awsome

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import App from './routes/app';
 import registerServiceWorker from './registerServiceWorker';
+
+import './styles/app.css';
+import './styles/css/font-awesome.min.css';
+import './styles/css/bootstrap.4.0.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
